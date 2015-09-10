e0036feff34eac4ca326d6a0a575a8306812f5de
James Munnelly <james.munnelly@gmail.com>
2015-09-10 23:07:02 +0100

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd $1
+mkdir -p "$1"
+ 
+cd "$1"
 
 echo "CD'd to directory: $1"
 
 echo "Executing with parameters: ${@:2}"
 
-/plexmediaserver/Resources/Plex\ New\ Transcoder "${@:2}"
+/plexmediaserver/Resources/Plex\ New\ Transcoder ${@:2}
