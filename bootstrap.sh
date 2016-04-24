05343744fbf49749ed9115ae8da16af7696d722e
James Munnelly <james.munnelly@gmail.com>
2016-04-24 22:20:36 +0100

    Don't quote arguments to Plex New Transcoder

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ echo "CD'd to directory: $1"
 
 echo "Executing with parameters: ${@:2}"
 
-/plexmediaserver/Resources/Plex\ New\ Transcoder "${@:2}"
+/plexmediaserver/Resources/Plex\ New\ Transcoder ${@:2}
