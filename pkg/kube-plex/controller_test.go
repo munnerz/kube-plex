b221b72b5c8484e3b665f499496be1a476aef936
Your Name <you@example.com>
2018-01-28 11:38:30 +0000

    add placeholder test

--- pkg/kube-plex/controller_test.go
+++ pkg/kube-plex/controller_test.go
@@ -2,16 +2,16 @@ package kubeplex
 
 import (
 	"testing"
-	fakeclientset "github.com/munnerz/kube-plex/pkg/client/clientset/versioned/fake"
+	fakekubeplex "github.com/munnerz/kube-plex/pkg/client/clientset/versioned/fake"
 	fakekubernetes "k8s.io/client-go/kubernetes/fake"
 )
 
 func TestController(t *testing.T) {
-	ptj := GeneratePlexTranscodeJob([]string{"/hello", "world"})
+	ptj := GeneratePlexTranscodeJob([]string{"/hello", "world"}, []string{})
 
 	kc := KubeClient{
-		Clientset: fakeclientset.NewSimpleClientset(ptj),
-		KubeplexClient: fakekubernetes.NewSimpleClientset(),
+		Clientset: fakekubernetes.NewSimpleClientset(),
+		KubeplexClient: fakekubeplex.NewSimpleClientset(&ptj),
 	}
 
 	NewController(&kc)
