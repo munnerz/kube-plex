a78b32dfdc0ff1444ce1dd0cb5c6725275ce5dfd
Sami Haahtinen <ressu@google.com>
2021-04-25 11:54:32 +0100

    Add tests for environment conversion

--- cmd/kube-plex/main_test.go
+++ cmd/kube-plex/main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 func Test_rewriter_Args(t *testing.T) {
@@ -32,6 +34,26 @@ func Test_rewriter_Args(t *testing.T) {
 			}
 			if !reflect.DeepEqual(orig, tt.args) {
 				t.Errorf("rewriter.Args() modifies input, orig: %v, after: %v", orig, tt.args)
+			}
+		})
+	}
+}
+
+func Test_toCoreV1EnvVar(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+		want []corev1.EnvVar
+	}{
+		{"handles empty environment", []string{}, []corev1.EnvVar{}},
+		{"splits single entries correctly", []string{"ENV=val"}, []corev1.EnvVar{{Name: "ENV", Value: "val"}}},
+		{"splits multiple entries", []string{"ENV=val", "ENV2=val2"}, []corev1.EnvVar{{Name: "ENV", Value: "val"}, {Name: "ENV2", Value: "val2"}}},
+		{"handles `=` in values", []string{"ENV=val=ue"}, []corev1.EnvVar{{Name: "ENV", Value: "val=ue"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toCoreV1EnvVar(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("toCoreV1EnvVar() = %v, want %v", got, tt.want)
 			}
 		})
 	}
