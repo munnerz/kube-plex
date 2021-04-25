2fbf48b81e3cad8fd6795bd040a4ca4647032bdc
Sami Haahtinen <ressu@google.com>
2021-04-25 15:32:22 +0100

    Break down code to separate files

--- cmd/kube-plex/kubernetes_test.go
+++ cmd/kube-plex/kubernetes_test.go
@@ -11,38 +11,6 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 )
 
-func Test_rewriter_Args(t *testing.T) {
-	r := rewriter{
-		pmsInternalAddress: "http://test-svc:32400",
-	}
-	tests := []struct {
-		name      string
-		args      []string
-		want      []string
-		ischanged bool
-	}{
-		{"unmodified args", []string{"-args", "arg1"}, []string{"-args", "arg1"}, false},
-		{"modifies loglevel", []string{"-s", "1", "-loglevel", "info"}, []string{"-s", "1", "-loglevel", "debug"}, true},
-		{"modifies plex loglevel", []string{"-s", "1", "-loglevel_plex", "info"}, []string{"-s", "1", "-loglevel_plex", "debug"}, true},
-		{"server address is adjusted", []string{"-progressurl", "http://127.0.0.1:32400/", "-l", "i"}, []string{"-progressurl", "http://test-svc:32400/", "-l", "i"}, true},
-		{"manifest url is adjusted", []string{"-manifest_name", "http://127.0.0.1:32400/manifest", "-l", "i"}, []string{"-manifest_name", "http://test-svc:32400/manifest", "-l", "i"}, true},
-		{"segment list url is adjusted", []string{"-segment_list", "http://127.0.0.1:32400/segments/url", "-l", "i"}, []string{"-segment_list", "http://test-svc:32400/segments/url", "-l", "i"}, true},
-		{"adjusts multiple arguments", []string{"-progressurl", "http://127.0.0.1:32400/", "-loglevel", "x"}, []string{"-progressurl", "http://test-svc:32400/", "-loglevel", "debug"}, true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			orig := make([]string, len(tt.args))
-			copy(orig, tt.args)
-			if got := r.Args(tt.args); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("rewriter.Args() = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(orig, tt.args) {
-				t.Errorf("rewriter.Args() modifies input, orig: %v, after: %v", orig, tt.args)
-			}
-		})
-	}
-}
-
 func Test_toCoreV1EnvVar(t *testing.T) {
 	tests := []struct {
 		name string
--- cmd/kube-plex/main.go
+++ cmd/kube-plex/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/munnerz/kube-plex/pkg/signals"
-	batch "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -95,150 +89,4 @@ func main() {
 	if err != nil {
 		klog.Exitf("Error cleaning up pod: %s", err)
 	}
-}
-
-type rewriter struct {
-	pmsInternalAddress string
-}
-
-// rewriteEnv rewrites environment variables to be passed to the transcoder
-func (r rewriter) Env(in []string) []string {
-	return in
-}
-
-// Args rewrites argument list to use kube-plex specific values
-func (r rewriter) Args(args []string) []string {
-	out := make([]string, len(args))
-	copy(out, args)
-	for i, v := range args {
-		switch v {
-		case "-progressurl", "-manifest_name", "-segment_list":
-			out[i+1] = strings.Replace(out[i+1], "http://127.0.0.1:32400", r.pmsInternalAddress, 1)
-		case "-loglevel", "-loglevel_plex":
-			out[i+1] = "debug"
-		}
-	}
-	return out
-}
-
-func generateJob(cwd string, env []string, args []string) *batch.Job {
-	envVars := toCoreV1EnvVar(env)
-	var ttl, backoff int32
-	ttl = int32((24 * time.Hour).Seconds())
-	backoff = 1
-	return &batch.Job{
-		TypeMeta: metav1.TypeMeta{},
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: "pms-elastic-transcoder-",
-		},
-		Spec: batch.JobSpec{
-			BackoffLimit:            &backoff,
-			TTLSecondsAfterFinished: &ttl,
-			Template: corev1.PodTemplateSpec{
-				Spec: corev1.PodSpec{
-					NodeSelector: map[string]string{
-						"beta.kubernetes.io/arch": "amd64",
-					},
-					RestartPolicy: corev1.RestartPolicyNever,
-					Containers: []corev1.Container{
-						{
-							Name:       "plex",
-							Command:    args,
-							Image:      pmsImage,
-							Env:        envVars,
-							WorkingDir: cwd,
-							VolumeMounts: []corev1.VolumeMount{
-								{Name: "data", MountPath: "/data", ReadOnly: true},
-								{Name: "transcode", MountPath: "/transcode"},
-							},
-						},
-					},
-					Volumes: []corev1.Volume{
-						{
-							Name: "data",
-							VolumeSource: corev1.VolumeSource{
-								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-									ClaimName: dataPVC,
-								},
-							},
-						},
-						{
-							Name: "transcode",
-							VolumeSource: corev1.VolumeSource{
-								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-									ClaimName: transcodePVC,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
-func toCoreV1EnvVar(in []string) []corev1.EnvVar {
-	out := make([]corev1.EnvVar, len(in))
-	for i, v := range in {
-		splitvar := strings.SplitN(v, "=", 2)
-		out[i] = corev1.EnvVar{
-			Name:  splitvar[0],
-			Value: splitvar[1],
-		}
-	}
-	return out
-}
-
-func waitForPodCompletion(ctx context.Context, cl kubernetes.Interface, job *batch.Job) error {
-	w, err := cl.BatchV1().Jobs(job.Namespace).Watch(ctx, metav1.SingleObject(job.ObjectMeta))
-	if err != nil {
-		return fmt.Errorf("failed to watch job: %v", err)
-	}
-	defer w.Stop()
-
-	// Check job state once before starting wait
-	j, err := cl.BatchV1().Jobs(job.Namespace).Get(ctx, job.Name, metav1.GetOptions{})
-	if err != nil {
-		return fmt.Errorf("unable to fetch job information for checking: %v", err)
-	}
-
-	if done, err := jobDone(j); done {
-		return err
-	}
-
-	return podWatcher(ctx, w)
-}
-
-func podWatcher(ctx context.Context, w watch.Interface) error {
-	for {
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("context cancelled: %v", ctx.Err())
-		case r := <-w.ResultChan():
-			switch r.Type {
-			case watch.Added:
-			case watch.Modified:
-				j := r.Object.(*batch.Job)
-
-				klog.V(2).Info("received an update for %s", j.Name)
-				if done, err := jobDone(j); done {
-					return err
-				}
-			case watch.Deleted:
-				j := r.Object.(*batch.Job)
-				klog.Error("Job %s deleted while waiting for it to complete!", j.Name)
-				return fmt.Errorf("job %s deleted unexpectedly", j.Name)
-			}
-		}
-	}
-}
-
-func jobDone(job *batch.Job) (bool, error) {
-	switch {
-	case job.Status.Failed > 0:
-		return true, fmt.Errorf("job %q failed", job.Name)
-	case job.Status.Succeeded > 0:
-		return true, nil
-	}
-	return false, nil
 }
